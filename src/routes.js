ac7b47b3c7fe1e8aee83365c0d37a8cb6cff0358
gibifyOfficial <evandrogibicoski@outlook.com>
2020-07-12 19:26:22 -0300

    beginning in the admin routes

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 
+const mainController = require('./app/controllers/main/mainController');
+const recipesController = require('./app/controllers/admins/recipesController');
 const data = require('../data');
 
 routes.get("/", function(req, res)  {
@@ -19,15 +21,9 @@ routes.get("/recipesList", function(req, res) {
     return res.render("recipesList", { recipes: data });
 });
 
-routes.get("/recipes/:index", function(req, res) {
-    const recipes = data;
-    const recipeIndex = req.params.index;
-        if(!recipes[recipeIndex]) {
-            return res.send("Recipe not found!")
-        }
+routes.get("/recipes/:index", mainController.show );
 
-        return res.render("recipes", { recipes: recipes[recipeIndex] });
-});
+routes.get("/admin/recipes", recipesController.create);
 
 
 module.exports = routes;
